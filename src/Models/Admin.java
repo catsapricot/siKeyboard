cf97a550bcc55270beb3c95690aaee846e46858c
Nazriel05 <arielkeren05@gmail.com>
2025-06-02 22:35:08 +0700

    editkatalog,dkk

--- src/Models/Admin.java
+++ src/Models/Admin.java
@@ -1,3 +1,5 @@
+import Models.Katalog;
+
 package Models;
 
 public class Admin extends Role {
@@ -30,4 +32,30 @@ public class Admin extends Role {
         return isValid;
     }
 
+    // Menghubungkan dengan Katalog.java
+    // Pastikan Anda sudah mengimport Katalog jika berada di package berbeda
+    // import Models.Katalog;
+
+    // Method untuk mengedit katalog
+    public void editKatalog(Katalog katalog, String namaProduk, String deskripsi, double harga, int stok, String gambarUrl, String rating) {
+        katalog.setNamaProduk(namaProduk);
+        katalog.setDeskripsi(deskripsi);
+        katalog.setHarga(harga);
+        katalog.setStok(stok);
+        katalog.setGambarUrl(gambarUrl);
+        katalog.setRating(rating);
+    }
+
+    public void tambahKatalog(java.util.List<Katalog> daftarKatalog, Katalog katalogBaru) {
+        daftarKatalog.add(katalogBaru);
+    }
+
+    public Katalog cariKatalog(java.util.List<Katalog> daftarKatalog, String namaProduk) {
+        for (Katalog katalog : daftarKatalog) {
+            if (katalog.getNamaProduk().equalsIgnoreCase(namaProduk)) {
+                return katalog;
+            }
+        }
+        return null;
+    }
 }
--- src/Models/Katalog.java
+++ src/Models/Katalog.java
@@ -9,5 +9,70 @@ public abstract class Katalog {
     private String gambarUrl;
     private String rating;
 
+    public Katalog(String idProduk, String namaProduk, String deskripsi, double harga, int stok, String gambarUrl, String rating) {
+        this.idProduk = idProduk;
+        this.namaProduk = namaProduk;
+        this.deskripsi = deskripsi;
+        this.harga = harga;
+        this.stok = stok;
+        this.gambarUrl = gambarUrl;
+        this.rating = rating;
+    }
+
+    public String getIdProduk() {
+        return idProduk;
+    }
+
+    public void setIdProduk(String idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    public String getGambarUrl() {
+        return gambarUrl;
+    }
+
+    public void setGambarUrl(String gambarUrl) {
+        this.gambarUrl = gambarUrl;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
 
